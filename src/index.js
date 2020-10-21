8af1a4694a8b208a72a5bbc6ac2305f5a498c0ac
helena-yuzhnaya <40338865+helena-yuzhnaya@users.noreply.github.com>
2020-10-21 20:33:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,8 +37,40 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) { 
+    let newArr = [];
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        newArr.push(expr.slice(i, i + 10));
+    }
+    for (let i = 0; i < newArr.length; i++) {
+        result += getLetter(newArr[i]);
+    }
+    return result;
+}
+
+function getLetter(code) {
+    if (code == '**********') {
+        return ' ';
+    }
+
+    let arr = [];
+    let morze = '';
+
+    for (let i = 0; i < 10; i + 2) {
+       arr.push(code.slice(i, i += 2));
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] == '10') {
+            morze += '.';
+        }
+        if(arr[i] == '11') {
+            morze += '-';
+        }
+    }
+
+    return MORSE_TABLE[morze];
 }
 
 module.exports = {
